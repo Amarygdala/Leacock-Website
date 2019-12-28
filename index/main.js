0905a1f859d0a6bcecc31420b377ffc0aa52bd4b
Xiang Weng <xiang.weng3@gmail.com>
2019-12-27 23:05:08 -0500

    Need to fix imgbtn prev logic

--- index/main.js
+++ index/main.js
@@ -29,6 +29,20 @@ document.getElementById("img-holder").onmouseleave= function(){
         bannerLoop();
     },transitionTime);
 }
+
+document.getElementById("imgnext").onclick = function(){
+    bannerLoop();
+}
+document.getElementById("imgprev").onclick = function(){
+    if(bannerStatus===1){
+        bannerStatus=2;
+    }else if(bannerStatus===2){
+        bannerStatus=3;
+    }else if(bannerStatus===3){
+        bannerStatus=1;
+    }
+    bannerBack();
+}
 function bannerLoop(){
     if(bannerStatus===1){
         img2.style.opacity = "0"
@@ -67,6 +81,51 @@ function bannerLoop(){
             img1.style.zIndex = "200";
             img2.style.right = "100%";
             img2.style.zIndex = "300";
+        }, 500);
+        setTimeout(function(){
+            img1.style.opacity = "1";
+        }, 1000);
+        bannerStatus = 1;
+        }
+}
+function bannerBack(){
+    if(bannerStatus===1){
+        img2.style.opacity = "0"
+        setTimeout(function(){
+            img3.style.right = "0px";
+            img3.style.zIndex = "100";
+            img2.style.right = "100%";
+            img2.style.zIndex = "200";
+            img1.style.right = "-100%";
+            img1.style.zIndex = "300";
+        }, 500);
+        setTimeout(function(){
+            img2.style.opacity = "1";
+        }, 1000);
+        bannerStatus = 2;
+    }else if(bannerStatus===2){
+        img3.style.opacity = "0"
+        setTimeout(function(){
+            img1.style.right = "0px";
+            img1.style.zIndex = "100";
+            img3.style.right = "100%";
+            img3.style.zIndex = "200";
+            img2.style.right = "-100%";
+            img2.style.zIndex = "300";
+        }, 500);
+        setTimeout(function(){
+            img3.style.opacity = "1";
+        }, 1000);
+        bannerStatus = 3;
+    }else if(bannerStatus===3){
+        img1.style.opacity = "0"
+        setTimeout(function(){
+            img2.style.right = "0px";
+            img2.style.zIndex = "100";
+            img1.style.right = "100%";
+            img1.style.zIndex = "200";
+            img3.style.right = "-100%";
+            img3.style.zIndex = "300";
         }, 500);
         setTimeout(function(){
             img1.style.opacity = "1";
